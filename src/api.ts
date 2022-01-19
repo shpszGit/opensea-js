91216bbc8d6ff326f87c6ce7187e1364301e3fae
何山 <62065532+970814925@users.noreply.github.com>
2022-01-19 20:42:12 +0800

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,21 @@
 import "isomorphic-unfetch";
 import * as QueryString from "query-string";
- import { getToken, removeToken } from './token.js';
+import Cookies from 'js-cookie'
+const key = 'uid'
+// 保存token
+export function saveToken(token) {
+  // 保存数据 token的值 是外部传入的
+  return Cookies.set(key, token)
+}
+// 获取token
+export function getToken() {
+  return Cookies.get(key)
+}
+// 删除token
+export function removeToken() {
+  Cookies.remove(key)
+}
+
 import {
   API_BASE_MAINNET,
   API_BASE_RINKEBY,
