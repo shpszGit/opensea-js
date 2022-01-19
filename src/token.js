2393edaff39b1ff1c2635557b70dd870fe753fec
何山 <62065532+970814925@users.noreply.github.com>
2022-01-19 21:15:18 +0800

    Update token.js

--- src/token.js
+++ src/token.js
@@ -1,15 +1,1 @@
-import Cookies from 'js.cookie'
-const key = 'uid'
-// 保存token
-export function saveToken(token) {
-  // 保存数据 token的值 是外部传入的
-  return Cookies.set(key, token)
-}
-// 获取token
-export function getToken() {
-  return Cookies.get(key)
-}
-// 删除token
-export function removeToken() {
-  Cookies.remove(key)
-}
+let token=123
