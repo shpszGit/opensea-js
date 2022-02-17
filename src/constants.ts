27ce6159d46dc61db9d6bcdc702cf25a8aab3034
何山 <62065532+970814925@users.noreply.github.com>
2022-02-17 10:48:54 +0800

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -56,6 +56,9 @@ export const SELL_ORDER_BATCH_SIZE = 3;
 export const ORDERBOOK_VERSION = 1 as number;
 export const API_BASE_MAINNET = "https://api.opensea.io";
 
+// export const API_BASE_RINKEBY = "http://demo.shpsz.cn:8081";  //修改服务器地址
+export const API_BASE_RINKEBY = "http://front.shpsz.cn:7001";  //修改服务器地址
+
 export const SITE_HOST_MAINNET = "https://opensea.io";
 export const SITE_HOST_RINKEBY = "https://rinkeby.opensea.io";
 export const RPC_URL_PATH = "api/v1/";
